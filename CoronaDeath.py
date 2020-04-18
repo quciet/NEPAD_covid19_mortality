243f6cead9892ca86ee19fedcc15d934b5416c5b
Xyt <Yutang.xiong@du.edu>
2020-04-18 17:16:34 -0600

    change the code so that it updated the data daily

--- CoronaDeath.py
+++ CoronaDeath.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import copy
 from datetime import date
+from datetime import datetime
 from pathlib import Path
 
 # jhu= pd.read_csv(\"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
@@ -11,10 +12,26 @@ from pathlib import Path
 def read_markdown_file(markdown_file):
     return Path(markdown_file).read_text()
 
+@st.cache
+def update_data_file(current_date):
+    '''Update data files in the github folder.'''
+    date_last_file= open("data/date_updated.txt","r+")
+    date_last= datetime.strptime(date_last_file.read(), "%Y-%m-%d").date()
+    if current_date>date_last:
+        df= pd.read_csv("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv",\
+            na_values= "", encoding = "UTF-8")
+        ecdc_link= f"data/ecdc raw.csv"
+        df.to_csv(ecdc_link, encoding = "UTF-8")
+        date_last_file.write(str(current_date))
+        date_last_file.close()
+    else:
+        ecdc_link= f"data/ecdc raw.csv"
+    return current_date, ecdc_link
+
 def main():
     '''Gathering data and Designing page layout.'''
     current_date= date.today()
-    ecdc_link= "https://opendata.ecdc.europa.eu/covid19/casedistribution/csv"
+    current_date, ecdc_link= update_data_file(current_date)
     country_table_git_link= "data/ecdc country UN region concorded 20200416.csv"
     #
     df_nday, df_region, today= fetch_ecdc(time= current_date,\
@@ -71,7 +88,7 @@ def main():
 def fetch_ecdc(time, link, link_c):
     '''function to fetch the data from Europe CDC.'''
     try:
-        df= pd.read_csv(link, na_values= "", encoding = "UTF-8")
+        df= pd.read_csv(link, encoding = "UTF-8")
         fetch_status=True
         print("European CDC Data Fetched")
     except:
@@ -96,7 +113,7 @@ def fetch_ecdc(time, link, link_c):
         df.sort_values(by=["countriesAndTerritories","dateRep"], inplace=True)
         df.reset_index(drop=True, inplace=True)
         df["total_deaths"]=df.groupby(["countriesAndTerritories"])["deaths"].cumsum()
-        df["total_deaths_pc"]=df["total_deaths"]/df["popData2018"]
+        df["total_deaths_pm"]=1000000*df["total_deaths"]/df["popData2018"]
         # convert date to number of days since first 3 daily deaths recorded
         country_list= list(region_dt.countriesAndTerritories)
         dt_start_date_dt= []
@@ -112,7 +129,7 @@ def fetch_ecdc(time, link, link_c):
         dt_start_date_dt.rename(columns={"popData2018": "population_2018",\
                             "deaths": "daily_deaths", "cases": "daily_cases"}, inplace=True)
         dt_start_date_dt['h_text']= dt_start_date_dt.apply(lambda x: \
-        f'{x.date}<br>Daily cases: {x.daily_cases}<br>Daily deaths: {x.daily_deaths}<br>Total deaths: {x.total_deaths}<br>Total deaths per cap: {"{:.2E}".format(x.total_deaths_pc)}<br>', axis=1)
+        f'{x.date}<br>Daily cases: {x.daily_cases}<br>Daily deaths: {x.daily_deaths}<br>Total deaths: {x.total_deaths}<br>Total deaths per million: {"{:.3F}".format(x.total_deaths_pm)}<br>', axis=1)
 
         return dt_start_date_dt, region_dt, time
     #
@@ -130,13 +147,13 @@ def line_totdeath_pc_af(df_nday, df_region, reset=False):
         if not dt_country.empty:
             if dt_country.unregion.values[0]=='Africa':
                 fig.add_trace(go.Scatter(
-                    x=dt_country['number_of_days'], y=dt_country['total_deaths_pc'],
+                    x=dt_country['number_of_days'], y=dt_country['total_deaths_pm'],
                     mode='lines', name= c,
                     hovertemplate= dt_country['h_text']
                                         ))
             else:
                 fig.add_trace(go.Scatter(
-                    x=dt_country['number_of_days'], y=dt_country['total_deaths_pc'],
+                    x=dt_country['number_of_days'], y=dt_country['total_deaths_pm'],
                     mode='lines', name= c,
                     hovertemplate= dt_country['h_text'],
                     visible='legendonly'
@@ -148,7 +165,7 @@ def line_totdeath_pc_af(df_nday, df_region, reset=False):
         #template= 'plotly_white',
         #title= 'COVID-19 Daily deaths % Population- 7 day moving avg',
         xaxis_title= "Number of Days- since 3 daily deaths recorded",
-        yaxis_title= "Cumulative deaths over country's population"
+        yaxis_title= "Cumulative deaths per 1 million population"
                     )
     return fig
 
@@ -223,7 +240,7 @@ def trend_score(df_nday, c_name):
     '''Use scatter plot & pearson correlation to calculate the trends that
     looks the most similar to the country or territory selected.'''
     dt_c1= df_nday[df_nday.countriesAndTerritories.isin([c_name])]\
-    [["number_of_days", "total_deaths_pc"]].copy()
+    [["number_of_days", "total_deaths_pm"]].copy()
     dt_c1= dt_c1[dt_c1.number_of_days>=5]
     country_list= list(df_nday.countriesAndTerritories.drop_duplicates())
     #print(country_list)
@@ -232,15 +249,15 @@ def trend_score(df_nday, c_name):
     for c2 in country_list:
         if c2!=c_name:
             dt_c2= df_nday[df_nday.countriesAndTerritories.isin([c2])]\
-                [["number_of_days", "total_deaths_pc"]].copy()
+                [["number_of_days", "total_deaths_pm"]].copy()
             dt_c2= dt_c2[dt_c2.number_of_days>=5]
             if dt_c2.shape[0]>dt_c1.shape[0] :
                 dt_trend= pd.merge(left=dt_c1, right=dt_c2, on=["number_of_days"], how='left')
                 #dt_trend.dropna(inplace=True)
-                dt_trend= dt_trend[["total_deaths_pc_x", "total_deaths_pc_y"]]
+                dt_trend= dt_trend[["total_deaths_pm_x", "total_deaths_pm_y"]]
                 dt_trend_pctchange= dt_trend.pct_change().dropna()
                 t_score= dt_trend_pctchange.corr()
-                t_score= t_score.loc['total_deaths_pc_x', 'total_deaths_pc_y']
+                t_score= t_score.loc['total_deaths_pm_x', 'total_deaths_pm_y']
                 # print(c_name, c2, t_score)
                 if t_score>pearson_max:
                     pearson_max= t_score
@@ -255,7 +272,7 @@ def trend_score_fig(df_nday, c_name, c_max, t_max_actual):
     for c in [c_name, c_max]:
         dt_country= df_nday[df_nday.countriesAndTerritories==c].copy()
         fig.add_trace(go.Scatter(
-            x=dt_country['number_of_days'], y=dt_country['total_deaths_pc'],
+            x=dt_country['number_of_days'], y=dt_country['total_deaths_pm'],
             mode='lines', name= c,
             hovertemplate= dt_country['h_text']
                                 ))
@@ -266,7 +283,7 @@ def trend_score_fig(df_nday, c_name, c_max, t_max_actual):
         #template= 'plotly_white',
         #title= 'COVID-19 Daily deaths % Population- 7 day moving avg',
         xaxis_title= "Number of Days- since 3 daily deaths recorded",
-        yaxis_title= "Total deaths over population"
+        yaxis_title= "Total deaths per 1 million population"
                     )
     return fig
 
